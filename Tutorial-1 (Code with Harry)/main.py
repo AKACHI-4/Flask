80a0f129837d6ff4f6bc928ca43c39d7b915d05f
AKACHI-4 <Adarshrawat8304@gmail.com>
2023-03-22 19:17:11 +0530

    Connect blog post app with database

--- Tutorial-1 (Code with Harry)/main.py
+++ Tutorial-1 (Code with Harry)/main.py
@@ -1,45 +1,71 @@
 from flask import Flask, render_template, request
 from flask_sqlalchemy import SQLAlchemy
+from flask_mail import Mail
+from datetime import datetime
+import json
+from sendMail import send_mail
 
 # Define app
+with open('config.json', 'r') as c: 
+    params = json.load(c)["params"]
+
+local_Server = params['local_server']
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://root:@localhost/codingthunder'
+# Ref. - flask-Mail documentation
+app.config.update (
+    MAIL_SERVER = 'smtp.gmail.com',
+    MAIL_PORT = '465', 
+    MAIL_USE_SSL = True,
+    MAIL_USERNAME = params['gmail-user'],
+    MAIL_PASSWORD = params['gmail-password']
+)
+
+mail = Mail(app)
+
+if(local_Server) :
+    app.config['SQLALCHEMY_DATABASE_URI'] = params['local_uri']
+else :
+    app.config['SQLALCHEMY_DATABASE_URI'] = params['prod_uri']
+
 db = SQLAlchemy(app)
 
 @app.route("/")
 def home() :
-    return render_template('index.html')
+    return render_template('index.html', params=params)
 
 @app.route("/about")
 def about() :
-    return render_template('about.html')
+    return render_template('about.html', params=params)
 
 class Contacts(db.Model):
     sno = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(80), nullable=False)
     phone_num = db.Column(db.String(12), unique=True, nullable=False)
-    msg = db.Column(db.String(120), nullable=False)
+    message = db.Column(db.String(120), nullable=False)
     date = db.Column(db.String(12), nullable=True)
-    email = db.Column(db.String(20), nullable=False)
+    email = db.Column(db.String(20), unique=True,nullable=False)
 
-@app.route("/contact", method = ['GET','POST'])
+@app.route("/contact", methods = ['GET','POST'])
 def contact() :
     if(request.method == 'POST') :
-        ''' Add entry to the database '''   
+        ''' Add entry to the database '''  
+        '''Fetch data and add it to the database''' 
         name = request.form.get('name')
         email = request.form.get('email')
         phone = request.form.get('phone_no')
         message = request.form.get('message')
 
-        entry = Contacts(name=name, phone_num=phone, msg=message, email=email)
+        entry = Contacts(name=name, phone_num=phone, message=message, date=datetime.now(), email=email)
 
         db.session.add(entry)
         db.session.commit()
 
-    return render_template('contact.html')
+        send_mail(customer, dealer, rating, comments); 
+
+    return render_template('contact.html', params=params)
 
 @app.route("/post")
 def post() :
-    return render_template('post.html')
+    return render_template('post.html', params=params)
 
 app.run(debug=True)
