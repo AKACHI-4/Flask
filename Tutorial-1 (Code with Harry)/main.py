98a94faaa10cb11d5160a4be6884a35f91fa0684
AKACHI-4 <Adarshrawat8304@gmail.com>
2023-03-31 20:06:05 +0530

    login feature added !!
    - Made a login dashboard to monitor user
    - Added add and edit post panel

--- Tutorial-1 (Code with Harry)/main.py
+++ Tutorial-1 (Code with Harry)/main.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, session, redirect
 from flask_sqlalchemy import SQLAlchemy
 from datetime import datetime
 import json
@@ -10,6 +10,7 @@ with open('config.json', 'r') as c:
 
 local_Server = params['local_server']
 app = Flask(__name__)
+app.secret_key = 'super-secret-key'
 
 if(local_Server) :
     app.config['SQLALCHEMY_DATABASE_URI'] = params['local_uri']
@@ -18,14 +19,30 @@ else :
 
 db = SQLAlchemy(app)
 
-@app.route("/login")
-def login() : 
-    return render_template('login.html', params=params)
+@app.route("/dashboard", methods=['GET','POST'])
+def dashboard() : 
+
+    if 'user' in session and session['user'] == params['admin_user'] :
+        posts = Posts.query.all() 
+        return render_template('dashboard.html', params=params, posts=posts)
+
+    if request.method == 'POST' :
+        # Redirect to admin panel 
+        username = request.form.get('uname') 
+        userpass = request.form.get('pass')
+        if(username == params['admin_user'] and userpass == params['admin_password']) :
+            # set the session variable
+            session['user'] = username
+            posts = Posts.query.all()
+
+            return render_template('dashboard.html', params=params, posts=posts)
+    else :
+        posts = Posts.query.all()
+        return render_template('login.html', params=params,  posts=posts)
 
 @app.route("/")
 def home() :
     posts = Posts.query.filter_by().all()[0:params['no_of_posts']]
-
     return render_template('index.html', params=params, posts=posts)
 
 @app.route("/about")
@@ -73,6 +90,38 @@ class Posts(db.Model):
 def post(post_slug) :
     post = Posts.query.filter_by(slug=post_slug).first()
     return render_template('post.html', params=params, post=post)
+
+@app.route("/edit/<string:sno>", methods = ['GET','POST'])
+def edit(sno) :
+    if 'user' in session and session['user'] == params['admin_user'] :
+        if request.method == 'POST':
+            box_title = request.form.get('title')
+            admin = request.form.get('admin')
+            tagline = request.form.get('tagline')
+            slug = request.form.get('slug')
+            content = request.form.get('content')
+            img_file = request.form.get('img_file')
+            date = datetime.now()
+
+            if sno == '0' :
+                post = Posts(title=box_title, admin=admin, slug=slug, content=content, tagline=tagline, img_file=img_file, date=date)
+                db.session.add(post)
+                db.session.commit()
+            else :
+                post = Posts.query.filter_by(sno=sno).first()
+                post.box_title = box_title
+                post.admin = admin
+                post.tline = tagline
+                post.slug = slug
+                post.content = content
+                post.img_file = img_file
+                post.date = date
+                db.session.commit()
+
+                return redirect('/edit/'+sno)
+
+        post = Posts.query.filter_by(sno=sno).first()
+        return render_template('edit.html', params=params, post=post)
 
 
 app.run(debug=True)
